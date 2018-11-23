4a3bf35bd2d73b1615bdf04df0f270585e427a47
Rooney Jung <rooney.jung@key-inside.com>
2018-11-23 21:05:04 +0900

    refactoring

--- stringset/stringset.go
+++ stringset/stringset.go
@@ -5,73 +5,81 @@ package stringset
 import "encoding/json"
 
 // A Set represents an unordered set of strings
-type Set map[string]bool
+type Set struct {
+	m map[string]bool
+}
 
 // New creates and returns a reference to an set
-func New(items ...string) Set {
-	set := make(Set)
-	for _, item := range items {
-		set[item] = true
-	}
+func New(items ...string) *Set {
+	set := &Set{make(map[string]bool, len(items))}
+	set.AppendSlice(items)
 	return set
 }
 
-// Add adds a string to the set
-func (s Set) Add(item string) {
-	if s != nil {
-		s[item] = true
+func (s *Set) safeMap() {
+	if nil == s.m {
+		s.m = make(map[string]bool)
 	}
 }
 
+// Add adds a string to the set
+func (s *Set) Add(item string) {
+	s.safeMap()
+	s.m[item] = true
+}
+
 // AppendSet adds strings of the 'set' to the set
-func (s Set) AppendSet(set Set) {
-	if s != nil {
-		for k := range set {
-			s[k] = true
-		}
+func (s *Set) AppendSet(set *Set) {
+	s.safeMap()
+	for k := range s.m {
+		s.m[k] = true
 	}
 }
 
 // AppendSlice adds strings from a slice to the set
-func (s Set) AppendSlice(items []string) {
-	if s != nil {
-		for _, item := range items {
-			s[item] = true
-		}
+func (s *Set) AppendSlice(items []string) {
+	s.safeMap()
+	for _, item := range items {
+		s.m[item] = true
 	}
 }
 
 // Contains reports whether item is within the set
-func (s Set) Contains(item string) bool {
-	return s[item]
+func (s *Set) Contains(item string) bool {
+	return s.m[item]
+}
+
+// Map returns strings map
+func (s *Set) Map() map[string]bool {
+	return s.m
 }
 
 // Remove removes a string from the set
-func (s Set) Remove(item string) {
-	delete(s, item)
+func (s *Set) Remove(item string) {
+	delete(s.m, item)
 }
 
 // Size returns the number of elements of the set
-func (s Set) Size() int {
-	return len(s)
+func (s *Set) Size() int {
+	return len(s.m)
 }
 
 // Strings returns the string array
-func (s Set) Strings() []string {
-	items := make([]string, 0, len(s))
-	for k := range s {
+func (s *Set) Strings() []string {
+	items := make([]string, 0, s.Size())
+	for k := range s.m {
 		items = append(items, k)
 	}
 	return items
 }
 
 // MarshalJSON implements the json.Marshaler interface
-func (s Set) MarshalJSON() ([]byte, error) {
+func (s *Set) MarshalJSON() ([]byte, error) {
 	return json.Marshal(s.Strings())
 }
 
 // UnmarshalJSON implements the json.Unmarshaler interface
-func (s Set) UnmarshalJSON(text []byte) error {
+func (s *Set) UnmarshalJSON(text []byte) error {
 	var items []string
 	if err := json.Unmarshal(text, &items); err != nil {
 		return err
