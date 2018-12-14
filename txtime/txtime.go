e67f69c14fa71c19ac99a9097a84a3a462321dd4
Rooney Jung <rooney.jung@key-inside.com>
2018-12-14 20:55:28 +0900

    FormatRFC3339Nano

--- txtime/txtime.go
+++ txtime/txtime.go
@@ -8,6 +8,10 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
+// RFC3339NanoFixed -
+// if unix time's nano seconds is 0, RFC3339Nano Format tails nano parts.
+const RFC3339NanoFixed = "2006-01-02T15:04:05.000000000Z"
+
 // GetTime returns the *Time converted from TxTimestamp
 func GetTime(stub shim.ChaincodeStubInterface) (*time.Time, error) {
 	ts, err := stub.GetTxTimestamp()
@@ -16,4 +20,9 @@ func GetTime(stub shim.ChaincodeStubInterface) (*time.Time, error) {
 	}
 	unixTime := time.Unix(ts.GetSeconds(), int64(ts.GetNanos()))
 	return &unixTime, nil
+}
+
+// FormatRFC3339Nano returns RFC3339Nano format string that forced nano parts
+func FormatRFC3339Nano(t time.Time) string {
+	return t.Format(RFC3339NanoFixed)
 }
