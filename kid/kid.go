9271c890524faa5badf9d5ae8508a86dac43bcce
Rooney Jung <rooney.jung@key-inside.com>
2019-04-16 21:53:20 +0900

    simple kid

--- kid/kid.go
+++ kid/kid.go
@@ -3,35 +3,16 @@
 package kid
 
 import (
-	"errors"
-	"os"
+	"encoding/hex"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"golang.org/x/crypto/sha3"
 )
 
-// KIDCfg is configuration for invoking KID chaincode
-var KIDCfg struct {
-	CC   string            // chaincode name
-	Args map[bool][][]byte // arguments bytes array
-}
-
-func init() {
-	if os.Getenv("DEV_CHANNEL_NAME") != "" {
-		KIDCfg.CC = "kiesnet-cc-id"
-	} else {
-		KIDCfg.CC = "kiesnet-id"
-	}
-	KIDCfg.Args = map[bool][][]byte{
-		false: [][]byte{[]byte("kid")},              // non-secure
-		true:  [][]byte{[]byte("kid"), []byte("1")}, // secure
-	}
-}
-
-// GetID invokes KID chaincode and returns the kiesnet ID.
+// GetID _
 func GetID(stub shim.ChaincodeStubInterface, secure bool) (string, error) {
-	res := stub.InvokeChaincode(KIDCfg.CC, KIDCfg.Args[secure], "")
-	if res.GetStatus() == 200 {
-		return string(res.GetPayload()), nil
-	}
-	return "", errors.New(res.GetMessage())
+	creator, _ := stub.GetCreator() // error is always nil
+	h := make([]byte, 20)
+	sha3.ShakeSum256(h, creator)
+	return hex.EncodeToString(h), nil
 }
